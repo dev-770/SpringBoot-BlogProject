3fde5a9a858da33b5582abd2b4730ea564331b33
haneu <rokap11111@gmail.com>
2021-07-28 01:53:18 +0900

    ajax 활용하여 json 변환

---
+++ blog-1/src/main/resources/static/js/user.js
@@ -0,0 +1,32 @@
+let index = {
+	init: function() {
+		$("#btn-save").on("click", ()=>{
+			this.save();
+		});
+	},
+	
+	save: function() {
+		let data = {
+			username: $("#username").val(),
+			password: $("#password").val(),
+			email: $("#email").val()
+		};
+
+		//console.log(data);
+		
+		$.ajax({
+			type: "POST",
+			url: "/blog/api/user",
+			data: JSON.stringify(data), // http bodydata
+			contentType: "application/json; charset=utf-8", // MINE TYPE
+			dataType: "json" // 요청 -> 응답 Default: String[JSON] => jsObject로 변경
+		}).done(function(resp){
+			alert("회원가입이 완료되었습니다.");
+			location.href = "/blog";
+		}).fail(function(error){
+			alert(JSON.stringify(error));
+		}); // ajax통신을 이용, json 변경->insert 요청
+	}
+}
+
+index.init();
