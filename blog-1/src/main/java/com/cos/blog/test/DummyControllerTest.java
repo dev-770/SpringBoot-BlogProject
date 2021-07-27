d497ad740c586e364bb61b23f717a278b35c1480
haneu <rokap11111@gmail.com>
2021-07-27 23:41:48 +0900

    전송 테스트

---
+++ blog-1/src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -0,0 +1,81 @@
+package com.cos.blog.test;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cos.blog.model.RoleType;
+import com.cos.blog.model.User;
+import com.cos.blog.repository.UserRepository;
+
+@RestController
+public class DummyControllerTest {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@DeleteMapping("/dummy/user/{id}")
+	private String delete(@PathVariable long id) {
+		try {
+			userRepository.deleteById(id);
+		} catch(EmptyResultDataAccessException e) {
+			return "삭제 실패 했습니다.";
+		}
+		return "삭제되었습니다. id : " + id ;
+	}
+	// save함수는 id전달X Insert, id전달O Update, id데이터X Insert
+	// Spring  json => java object 변환
+	// @Transactional // 함수종료시 Commit
+	@PutMapping("/dummy/user/{id}")
+	public User updateUser(@PathVariable long id, @RequestBody User requestUser) {
+		User user = userRepository.findById(id).orElseThrow(()->{
+			return new IllegalArgumentException("수정에 실패 했습니다.");
+		});
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());
+		
+		requestUser.setId(id);
+		userRepository.save(user);
+		return user;
+	}
+	
+	@GetMapping("/dummy/users")
+	public List<User> list() {
+		return userRepository.findAll();
+	}
+	
+	@GetMapping("/dummy/user")
+	public List<User> pageList(@PageableDefault(size=2, sort="id", direction = Sort.Direction.DESC) Pageable pageable) {
+		Page<User> pagingUser = userRepository.findAll(pageable);
+		
+		List<User> users = pagingUser.getContent();
+		return users;
+	}
+
+	@GetMapping("/dummy/user/{id}")
+	public User detail(@PathVariable long id) {
+		User user = userRepository.findById(id).orElseThrow(()->{
+			return new IllegalArgumentException("해당 유저는 없습니다. id : " + id);
+		});
+		return user;
+	}
+	
+	@PostMapping("/dummy/join")
+	public String join(User user) { // key = value
+		user.setRole(RoleType.USER);
+		userRepository.save(user);
+		return "회원가입이 완료되었습니다.";
+	}
+}
