fa53df9cd66e17fa6466d692503343c72ca8252f
haneu <rokap11111@gmail.com>
2021-07-28 18:23:54 +0900

    유저 DB 처리

--- blog-1/src/main/java/com/cos/blog/repository/UserRepository.java
+++ blog-1/src/main/java/com/cos/blog/repository/UserRepository.java
@@ -1,10 +1,19 @@
 package com.cos.blog.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cos.blog.model.User;
 
 // DAO 자동으로 Bean 등록
 public interface UserRepository extends JpaRepository<User, Long> {
-
+	// JPA Naming 쿼리 (select * from user where username = ?1 and password = ?2;)
+	User findByUsernameAndPassword(String username, String password);
+	
+	// (select * from user where username = ?1;
+	Optional<User> findByUsername(String username);
+	
+//	@Query(value="select * from username = ?1 and password = ?2;", nativeQuery = true)
+//	User login(String username, String password);
 }
