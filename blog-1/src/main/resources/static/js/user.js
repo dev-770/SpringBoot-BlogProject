e77445cba2bf29b81392443ec2d935ae29402c72
haneu <rokap11111@gmail.com>
2021-07-28 18:24:49 +0900

    ajax 데이터 송신

--- blog-1/src/main/resources/static/js/user.js
+++ blog-1/src/main/resources/static/js/user.js
@@ -16,13 +16,13 @@ let index = {
 		
 		$.ajax({
 			type: "POST",
-			url: "/blog/api/user",
+			url: "/auth/joinProc",
 			data: JSON.stringify(data), // http bodydata
 			contentType: "application/json; charset=utf-8", // MINE TYPE
 			dataType: "json" // 요청 -> 응답 Default: String[JSON] => jsObject로 변경
 		}).done(function(resp){
 			alert("회원가입이 완료되었습니다.");
-			location.href = "/blog";
+			location.href = "/";
 		}).fail(function(error){
 			alert(JSON.stringify(error));
 		}); // ajax통신을 이용, json 변경->insert 요청
@@ -30,3 +30,4 @@ let index = {
 }
 
 index.init();
+
