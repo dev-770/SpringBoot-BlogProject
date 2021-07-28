438337eb628915eda3c8dea5b3f51c71093d9a17
haneu <rokap11111@gmail.com>
2021-07-28 18:19:12 +0900

    유저 컨트롤러

--- blog-1/src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ blog-1/src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -1,5 +1,6 @@
 package com.cos.blog.controller.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,12 +8,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.dto.ResponseDto;
 import com.cos.blog.model.User;
+import com.cos.blog.service.UserService;
 
 @RestController
 public class UserApiController {
+	
+	@Autowired
+	private UserService userService;
 
-	@PostMapping("/api/user")
+	@PostMapping("/auth/joinProc")
 	public ResponseDto<Integer> save(@RequestBody User user) {
-		return new ResponseDto<Integer>(HttpStatus.OK, 1); // javaObject를 JSON 변환 (Jackson)
+		userService.join(user);
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); // javaObject를 JSON 변환 (Jackson)
 	}
+	
+	/*
+	 * @PostMapping("/auth/user/login") public ResponseDto<Integer>
+	 * login(@RequestBody User user, HttpSession session) { User principal =
+	 * userService.login(user); if (principal != null) {
+	 * session.setAttribute("principal", principal); } return new
+	 * ResponseDto<Integer>(HttpStatus.OK.value(), 1); }
+	 */
 }
