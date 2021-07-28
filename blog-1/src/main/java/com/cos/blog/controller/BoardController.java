7bd5ca23e2ac5873f007446a6e0aad2b3903d124
haneu <rokap11111@gmail.com>
2021-07-28 18:16:55 +0900

    게시판 화면 컨트롤러

--- blog-1/src/main/java/com/cos/blog/controller/BoardController.java
+++ blog-1/src/main/java/com/cos/blog/controller/BoardController.java
@@ -5,9 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
-
+	
 	@GetMapping({"","/"})
 	public String indexMain() {
 		return "index";
+	}
+	
+	@GetMapping("/board/writeForm")
+	public String writeForm() {
+		return "board/writeForm";
 	}
 }
