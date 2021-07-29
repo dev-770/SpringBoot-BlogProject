60585e2b8121b85c5241588bf7ed30a0476bf508
haneu <rokap11111@gmail.com>
2021-07-29 23:42:16 +0900

    서비스

--- blog-1/src/main/java/com/cos/blog/service/BoardService.java
+++ blog-1/src/main/java/com/cos/blog/service/BoardService.java
@@ -2,6 +2,8 @@ package com.cos.blog.service;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,5 +22,33 @@ public class BoardService {
 		board.setCount(0);
 		board.setUser(user);
 		boardRepository.save(board);
+	}
+	
+	@Transactional (readOnly = true)
+	public Page<Board> list(Pageable pageable) {
+		return boardRepository.findAll(pageable);
+	}
+	
+	@Transactional (readOnly = true)
+	public Board detail(int id) {
+		return boardRepository.findById(id)
+				.orElseThrow(()->{
+					return new IllegalArgumentException("Error : 글을 찾을 수 없습니다.");
+				});
+	}
+	
+	@Transactional
+	public void update(int id, Board reqboard) {
+		Board board = boardRepository.findById(id)
+				.orElseThrow(()->{
+					return new IllegalArgumentException("Error : 글을 찾을 수 없습니다.");
+				}); // 영속화
+		board.setTitle(reqboard.getTitle());
+		board.setContent(reqboard.getContent());
+	}
+	
+	@Transactional
+	public void delete(int id) {
+		boardRepository.deleteById(id);
 	}
 }
--- blog-1/src/main/java/com/cos/blog/service/UserService.java
+++ blog-1/src/main/java/com/cos/blog/service/UserService.java
@@ -28,6 +28,18 @@ public class UserService {
 		userRepository.save(user);
 	}
 	
+	@Transactional
+	public void userUpdate(int id, User user) {
+		User persistance = userRepository.findById(id).orElseThrow(()->{
+			return new IllegalArgumentException("Error : 유저를 찾을 수 없습니다.");
+		});
+		String rawPassword = user.getPassword();
+		String endPassword = encoder.encode(rawPassword);
+		persistance.setPassword(endPassword);
+		persistance.setEmail(user.getEmail());
+		
+	}
+	
 	// Select 트랜잭션 시작, 서비스 종료시 트랜잭션 종료
 	/*
 	 * @Transactional(readOnly = true) public User login (User user) {
