58f3d0e2b386a281fe93fc4dfa4f872bd77f943a
haneu <rokap11111@gmail.com>
2021-07-30 22:07:03 +0900

    게시판 api 컨트롤러

--- blog-1/src/main/java/com/cos/blog/controller/api/BoardApiController.java
+++ blog-1/src/main/java/com/cos/blog/controller/api/BoardApiController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.auth.PrincipalDetail;
+import com.cos.blog.dto.BoardDto;
+import com.cos.blog.dto.ReplyDto;
 import com.cos.blog.dto.ResponseDto;
 import com.cos.blog.model.Board;
 import com.cos.blog.service.BoardService;
@@ -22,7 +24,7 @@ public class BoardApiController {
 	private BoardService boardService;
 	
 	@PostMapping("/api/board") // Getter로 받아서 유저 정보를 넘김
-	public ResponseDto<Integer> save(@RequestBody Board board, @AuthenticationPrincipal PrincipalDetail principal) {
+	public ResponseDto<Integer> insert(@RequestBody Board board, @AuthenticationPrincipal PrincipalDetail principal) {
 		boardService.write(board, principal.getUser());
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
@@ -34,8 +36,14 @@ public class BoardApiController {
 	}
 	
 	@PutMapping("/api/board/{id}")
-	public ResponseDto<Integer> deleteById(@PathVariable int id, @RequestBody Board board) {
-		boardService.update(id, board);
+	public ResponseDto<Integer> deleteById(@RequestBody BoardDto boardDto) {
+		boardService.update(boardDto);
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+	}
+	
+	@PostMapping("/api/board/{boardId}/reply")
+	public ResponseDto<Integer> replyInsert(@RequestBody ReplyDto replyDto) {
+		boardService.replyWrite(replyDto);
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
 }
