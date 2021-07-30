e137c36e32c6cbacf1f6ff837a2e2635d8643760
haneu <rokap11111@gmail.com>
2021-07-30 22:06:13 +0900

    게시판 서비스

--- blog-1/src/main/java/com/cos/blog/service/BoardService.java
+++ blog-1/src/main/java/com/cos/blog/service/BoardService.java
@@ -7,15 +7,26 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.cos.blog.dto.BoardDto;
+import com.cos.blog.dto.ReplyDto;
 import com.cos.blog.model.Board;
+import com.cos.blog.model.Reply;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.BoardRepository;
+import com.cos.blog.repository.ReplyRepository;
+import com.cos.blog.repository.UserRepository;
 
 @Service
 public class BoardService {
-
+	
 	@Autowired
 	private BoardRepository boardRepository;
+	
+	@Autowired
+	private ReplyRepository replyRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
 	
 	@Transactional 
 	public void write(Board board, User user) {  // title, content, user
@@ -38,17 +49,37 @@ public class BoardService {
 	}
 	
 	@Transactional
-	public void update(int id, Board reqboard) {
-		Board board = boardRepository.findById(id)
+	public void update(BoardDto boardDto) {
+		Board board = boardRepository.findById(boardDto.getId())
 				.orElseThrow(()->{
 					return new IllegalArgumentException("Error : 글을 찾을 수 없습니다.");
 				}); // 영속화
-		board.setTitle(reqboard.getTitle());
-		board.setContent(reqboard.getContent());
+		board.setTitle(boardDto.getTitle());
+		board.setContent(boardDto.getContent());
 	}
 	
 	@Transactional
 	public void delete(int id) {
 		boardRepository.deleteById(id);
+	}
+	
+	@Transactional
+	public void replyWrite (ReplyDto replyDto) {
+		
+		User user = userRepository.findById(replyDto.getUserId()).orElseThrow(()->{
+			return new IllegalArgumentException("Error : 유저 id를 찾을 수 없습니다.");
+		}); 
+		
+		Board board = boardRepository.findById(replyDto.getBoardId()).orElseThrow(()->{
+			return new IllegalArgumentException("Error : 게시글 id를 찾을 수 없습니다.");
+		}); 
+		
+		Reply reply = Reply.builder()
+				.user(user)
+				.board(board)
+				.content(replyDto.getContent())
+				.build();
+
+		replyRepository.save(reply);
 	}
 }
