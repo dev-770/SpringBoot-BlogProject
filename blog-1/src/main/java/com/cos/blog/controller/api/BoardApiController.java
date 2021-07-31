420fee3f478342c14d9034dc0c2693e2b327f279
haneu <rokap11111@gmail.com>
2021-07-31 17:43:21 +0900

    게시판 api 컨트롤러

--- blog-1/src/main/java/com/cos/blog/controller/api/BoardApiController.java
+++ blog-1/src/main/java/com/cos/blog/controller/api/BoardApiController.java
@@ -46,4 +46,11 @@ public class BoardApiController {
 		boardService.replyWrite(replyDto);
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
+	
+	@DeleteMapping("/api/board/{boardId}/reply/{replyId}")
+	public ResponseDto<Integer> replyDelete(@PathVariable int replyId) {
+		System.out.println(replyId);
+		boardService.replyDelete(replyId);
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+	}
 }
