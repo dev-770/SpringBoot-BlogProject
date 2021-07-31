0743f7df6bf6fc951f397a653477c4eca8e2ccf0
haneu <rokap11111@gmail.com>
2021-07-31 17:31:30 +0900

    회원 ajax 처리

--- blog-1/src/main/resources/static/js/user.js
+++ blog-1/src/main/resources/static/js/user.js
@@ -24,8 +24,12 @@ let index = {
 			contentType: "application/json; charset=utf-8", // MINE TYPE
 			dataType: "json" // 요청 -> 응답 Default: String[JSON] => jsObject로 변경
 		}).done(function(resp){
-			alert("회원가입이 완료되었습니다.");
-			location.href = "/";
+			if(resp.status === 500) {
+				alert("회원가입이 실패했습니다..");
+			}else {
+				alert("회원가입이 완료되었습니다.");
+				location.href = "/";
+			}
 		}).fail(function(error){
 			alert(JSON.stringify(error));
 		}); // ajax통신을 이용, json 변경->insert 요청
