2f48fdf43c297e51921dedd4fdb9cccb0c2a3cdd
haneu <rokap11111@gmail.com>
2021-07-31 17:43:36 +0900

    게시판 Dto

--- blog-1/src/main/java/com/cos/blog/dto/BoardDto.java
+++ blog-1/src/main/java/com/cos/blog/dto/BoardDto.java
@@ -10,5 +10,10 @@ import lombok.NoArgsConstructor;
 public class BoardDto {
 	private int id;
 	private String title;
-	private String content;;
+	private String content;
+	
+	@Override
+	public String toString() {
+		return "BoardDto [id=" + id + ", title=" + title + ", content=" + content + "]";
+	}
 }
